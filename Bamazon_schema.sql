baa6fbe7c499a45fa7c3c7c2604d533c879a8bb9
Juan John-Charles <whoisju1@gmail.com>
2017-04-10 21:31:47 -0400

    table created and populated, and connection made to database in js file

--- Bamazon_schema.sql
+++ Bamazon_schema.sql
@@ -1,11 +1,28 @@
 USE Bamazon;
 CREATE table products (
-	item_id integer (20) not null auto_increment,
+	item_id integer (20) auto_increment,
     product_name varchar (100),
     department_name varchar (100),
-    price decimal (65),
-    stock_quanitiy decimal (65),
+    price decimal (65, 2),
+    stock_quanitiy integer (65),
     primary key (item_id)
     );
     
-    
+INSERT INTO products VALUES (1, "ESV Study Bible", "Books", 60.00, 57);
+INSERT INTO products VALUES (2, "Sol de Janeiro Brazilian Crush Body Fragrance Mist", "Beauty", 65.00, 3);
+INSERT INTO products VALUES (3, "Cuties Baby Diapers", "Baby", 18.80, 51);
+INSERT INTO products VALUES (4, "Eloquent Javascript", "Books", 29.99, 65);
+INSERT INTO products VALUES (5, "Pulleez Sliding Hair Tiesd", "Kids", 45.00, 49);
+INSERT INTO products VALUES (6, "Viva Naturals Premium Non-GMO Vitamin C ", "Health", 41.00, 1);
+INSERT INTO products VALUES (7, "Flutter & Link Friend Take-Along Toy", "Kids", 11.00, 23);
+INSERT INTO products VALUES (8, "For Honor", "Games", 99.00, 61);
+INSERT INTO products VALUES (9, "Alalisien-R", "Shoes", 125.00, 38);
+INSERT INTO products VALUES (10, "Kong: Skull Island", "Movies", 11.95, 38);
+
+select * from products;
+
+
+
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,24 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+
+
+    password: "",
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+connection.query("select * from products", function(err, results) {
+    if (err) throw err;
+
+    console.log(results[0]);
+});
